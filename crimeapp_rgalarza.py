d5cf98350bf468b11957b36d7e6c86da576d6c15
raychangalarza <raychangalarza@gmail.com>
2026-05-19 12:52:30 -0400

    Added logo and started coding crimeapp_rgalarza.py

--- crimeapp_rgalarza.py
+++ crimeapp_rgalarza.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+
+# Title 
+st.logo("crimeapp_logo.png")
+st.title("Dashboard de Datos de Crímenes en Puerto Rico")
+
+# Creating dataframe
+@st.cache_data
+def load_data(data):
+    df = pd.read_csv(data)
+    return df
+
+df = load_data("crime_processed.csv")
+
