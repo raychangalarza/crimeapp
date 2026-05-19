b1bc8ac368f183a63a33c08a16bdc7be77fdc2f1
raychangalarza <raychangalarza@gmail.com>
2026-05-19 13:07:02 -0400

    .

--- crimeapp_rgalarza.py
+++ crimeapp_rgalarza.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.express as px
 
 # Title 
-st.logo("crimeapp_logo.png")
+st.logo("crimeapp_logo.png", size='large')
 st.title("Dashboard de Datos de Crímenes en Puerto Rico")
 
 # Creating dataframe
@@ -14,3 +14,29 @@ def load_data(data):
 
 df = load_data("crime_processed.csv")
 
+area_policiaca = st.sidebar.selectbox(df["Area"].unique())
+
+
+def dameIndice(delito):
+  if delito == "Asesinato":
+    return 10
+  elif delito == "Violacion":
+    return 9.5
+  elif delito == "Trata Humana":
+    return 9.0
+  elif delito == "Incendio Malicioso":
+    return 8.5
+  elif delito == "Agresion Agravada":
+    return 8.0
+  elif delito == "Robo":
+    return 7.0
+  elif delito == "Escalamiento":
+    return 6.0
+  elif delito == "Vehiculo Hurtado":
+    return 5.5
+  elif delito == "Apropiacion Ilegal":
+    return 5.0
+  else:
+    return 4
+  
+crimenes = df["Crime"].unique().tolist()
